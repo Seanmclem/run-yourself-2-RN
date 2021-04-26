234ef2bd4d3dff0d487d5d44afef2f21b44ba4e1
Seanmclem <seanmclements@gmail.com>
2021-04-26 17:31:48 -0400

    basic RN paper bottom nav working

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -3,27 +3,35 @@
  * https://reactnavigation.org/docs/bottom-tab-navigator
  */
 
-import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 
-import Colors from '../constants/Colors';
+// import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
+
 import RunTimerScreen from '../screens/RunTimerScreen';
 import HistoryScreen from '../screens/HistoryScreen';
-import { BottomTabParamList } from '../types';
+// import { BottomTabParamList } from '../types';
 import { Appbar, Avatar } from 'react-native-paper';
-import { TouchableOpacity, Text } from 'react-native';
 
-const Header = ({ scene, previous, navigation }: any) => {
+// import { TouchableOpacity, Text } from 'react-native';
+
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+const Tab = createMaterialBottomTabNavigator();
+
+
+export const Header = ({ scene, previous, navigation }: any) => {
   const { options } = scene.descriptor;
-  const title =
-    options.headerTitle !== undefined
-      ? options.headerTitle
-      : options.title !== undefined
-        ? options.title
-        : scene.route.name;
+  // const title =
+  //   options.headerTitle !== undefined
+  //     ? options.headerTitle
+  //     : options.title !== undefined
+  //       ? options.title
+  //       : scene.route.name;
 
   return (
     <Appbar.Header theme={{ colors: { primary: 'purple' } }}>
@@ -59,57 +67,59 @@ const Header = ({ scene, previous, navigation }: any) => {
   );
 };
 
-const BottomTabs = createStackNavigator<BottomTabParamList>();
+// const Stack = createStackNavigator<BottomTabParamList>();
 
 export default function BottomTabNavigator() {
 
-  return (
-    <BottomTabs.Navigator
+  return ( //replace contents with myTabs contents
+    <Tab.Navigator
       initialRouteName="Run Timer"
-      headerMode="screen"
-      screenOptions={{
-        header: ({ scene, previous, navigation }) => (
-          <Header scene={scene} previous={previous} navigation={navigation} />
-        ),
-      }}
+      activeColor="#FFF"
+      // labelStyle={{ fontSize: 12 }}
+      // style={{ backgroundColor: 'tomato' }}
+      shifting={true}
     >
-      <BottomTabs.Screen
-        name="Run Timer"
-        component={RunTimerScreen}
-        options={{ headerTitle: 'Run Timer' }}
-      />
-      <BottomTabs.Screen
+      <Tab.Screen
         name="History"
         component={HistoryScreen}
-        options={{ headerTitle: 'History' }}
+        options={{
+          tabBarLabel: 'History',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="format-list-bulleted" color={color} size={26} />
+          ),
+        }}
       />
-    </BottomTabs.Navigator>
+      <Tab.Screen
+        name="Run Timer"
+        component={RunTimerScreen}
+        options={{
+          tabBarLabel: 'Running',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="clock" color={color} size={26} />
+          ),
+        }}
+      />
+
+    </Tab.Navigator>
+    // <Stack.Navigator
+    //   initialRouteName="Run Timer"
+    //   headerMode="screen"
+    //   screenOptions={{
+    //     header: ({ scene, previous, navigation }) => (
+    //       <Header scene={scene} previous={previous} navigation={navigation} />
+    //     ),
+    //   }}
+    // >
+    //   <Stack.Screen
+    //     name="Run Timer"
+    //     component={RunTimerScreen}
+    //     options={{ headerTitle: 'Run Timer' }}
+    //   />
+    //   <Stack.Screen
+    //     name="History"
+    //     component={HistoryScreen}
+    //     options={{ headerTitle: 'History' }}
+    //   />
+    // </Stack.Navigator>
   );
 }
-
-// You can explore the built-in icon families and icons on the web at:
-// https://icons.expo.fyi/
-// function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
-//   return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
-// }
-
-// Each tab has its own navigation stack, you can read more about this pattern here:
-// https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
-// const Stack = createStackNavigator<RunTimerScreenTabParamList>();
-
-// function RunTimerScreenTabNavigator() {
-//   return (
-//     <Stack.Navigator>
-//       <Stack.Screen
-//         name="RunTimerScreen"
-//         component={RunTimerScreen}
-//         options={{ headerTitle: 'Run Timer' }}
-//       />
-//       <Stack.Screen
-//         name="c"
-//         component={HistoryScreen}
-//         options={{ headerTitle: 'Run History' }}
-//       />
-//     </Stack.Navigator>
-//   );
-// }
